9548ed112dc2e7e6ae1a7380b3992edd8d014723
Ana Clara Ferreira <anaclara358@yahoo.com.br>
2021-06-30 11:44:35 -0300

    Alterar Foto

--- resources/views/pages/photo_form.blade.php
+++ resources/views/pages/photo_form.blade.php
@@ -6,21 +6,28 @@
   <!-- Coluna btn Voltar -->
   <div class="row">
     <div class="col-12 my-4">
-      <a href=""><i class="fas fa-arrow-left me-2"></i>Voltar</a>
+      <a href="/"><i class="fas fa-arrow-left me-2"></i>Voltar</a>
     </div>
 
     <!-- Coluna card form -->
     <div class="col-12">
       <div class="card shadow bg-white rounded">
         <div class="card-header gradient text-white">
-          <h2 class="card-title p-3"><i class="fas fa-image"></i> Nova Imagem</h2>
+          <h2 class="card-title p-3"><i class="fas fa-image"></i>{{isset($photo) ? 'Alterar Foto' : 'Nova Foto'}}</h2>
         </div>
 
         <div class="card-body p-4">
-            <form action="/photos" method="POST">
+            {{--  --}}
+            @if (isset($photo))
+              <form action="/photos/{{$photo->id}}" method="POST">
+              @method('PUT')
+            @else
+              <form action="/photos" method="POST">
+            @endif
             @csrf
 
               <div class="row">
+
               <!-- Coluna da imagem -->
                 <div class="col-lg-6">
                   <div class="d-flex flex-column h-100">
@@ -45,8 +52,8 @@
                       <div class="input-group">
                         <div class="input-group-text"> <i class="fas fa-image"></i>
                         </div>
-                        <input id="title" name="title" type="text" class="form-control"
-                          placeholder="Digite o título da sua imagem">
+                        <input id="title" name="title" type="text" class="form-control" required
+                          placeholder="Digite o título da sua imagem" value="{{$photo->title ?? null }}">
                       </div>
                   </div>
 
@@ -56,15 +63,16 @@
                       <div class="input-group">
                           <div class="input-group-text"> <i class="far fa-calendar-alt"></i>
                           </div>
-                        <input id="date" name="date" type="date" class="form-control">
+                        <input id="date" name="date" type="date" class="form-control" required
+                         value="{{$photo->date ?? null }}">
                       </div>
                   </div>
 
                   <!-- Descrição -->
                   <div class="form-group mt-3">
                     <label for="description">Descrição</label>
-                    <textarea id="description" name="description" cols="40" rows="5" class="form-control"
-                      placeholder="Digite uma pequena descrição da imagem"></textarea>
+                    <textarea id="description" name="description" cols="40" rows="5" class="form-control" required
+                      placeholder="Digite uma pequena descrição da imagem">{{$photo->description ?? null}}</textarea>
                   </div>
 
                   <!-- Botões -->
